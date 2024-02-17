ba81bcf6ca46b75a79fa365a46b6ebba044181ea
PaBoba <140555335+PaBoba@users.noreply.github.com>
2024-02-16 22:19:26 -0800

    added in reaction api routes

---
+++ routes/api/thoughts/reactions/add-reaction.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const { addReaction } = require("../../../../controllers/reaction");
+
+router.post("/", addReaction);
+
+module.exports = router;
---
+++ routes/api/thoughts/reactions/delete-reaction.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const { deleteReaction } = require("../../../../controllers/reaction");
+
+router.delete("/:reactionId", deleteReaction);
+
+module.exports = router;
